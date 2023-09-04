79f9fdb7f447e0fdb1ba0bcdd1081f2f6f8dab9c
Felipe Hernandez <fhernandez@beyondtrust.com>
2023-09-04 12:51:47 -0500

    first commit

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import os
 print("Python Execution")
-token = os.getenv('INPUT_GITHUB-TOKEN')
-who = os.getenv('INPUT_WHO-TO-GREET')
+token = os.getenv('INPUT_GITHUB_TOKEN')
 print(f"Got token {token}")
-print(f"who {who}")
 
+
+import requests
+
+url = 'https://api.github.com/orgs/gitahernandezorg/actions/secrets'
+headers = {'Authorization': 'token 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'}
+
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print('Error al hacer la solicitud:', response.status_code)
