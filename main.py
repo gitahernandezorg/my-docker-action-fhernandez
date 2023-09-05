7e5bc2aac92eb9447ff1f97943900a2c8fa606ca
Felipe Hernandez <fhernandez@beyondtrust.com>
2023-09-05 18:59:27 -0500

    test

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ greet_env = os.getenv('who-to-greet-env')
 print(f"Got token {token}")
 print(f"greet_env {greet_env}")
 
+
+def set_github_action_output(output_name, output_value):
+    f = open(os.path.abspath(os.environ["GITHUB_OUTPUT"]), "a")
+    f.write(f'{output_name}={output_value}')
+    f.close()
+    
+
 """"
 import requests
 
@@ -20,4 +27,5 @@ else:
     print('Error al hacer la solicitud:', response.status_code)
 """
 num = 2 * 2
-print(f"::set-output name=test_variable::{num}")
+set_github_action_output('test_variable', num)
+
