017e5d27366cd75709830a7764facfc5b27ed9e6
Felipe Hernandez <fhernandez@beyondtrust.com>
2023-09-06 08:22:48 -0500

    test

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import os
+import base64
+import requests
+import uuid
+
+
 print("Python Execution")
 token = os.getenv('INPUT_GITHUB_TOKEN')
 greet_env = os.getenv('who-to-greet-env')
@@ -10,11 +15,9 @@ def set_github_action_output(output_name, output_value):
     f = open(os.path.abspath(os.environ["GITHUB_OUTPUT"]), "a")
     f.write(f'{output_name}={output_value}')
     f.close()
-    
+
 
 """"
-import requests
-
 url = 'https://api.github.com/orgs/gitahernandezorg/actions/secrets'
 headers = {'Authorization': f"token {token}"}
 
@@ -29,3 +32,48 @@ else:
 num = 2 * 2
 set_github_action_output('test_variable', num)
 
+multiline = "geeksy\
+for\
+geeks"
+set_github_action_output('multiline', multiline)
+
+random_uuid = uuid.uuid4()
+
+# Definir los parámetros necesarios
+secret_name = "my_secret_"+str(random_uuid).replace("-", "")
+secret_value = multiline  # El valor del secreto que deseas almacenar
+
+# URL de la API de GitHub para crear el secreto
+url = f"https://api.github.com/orgs/gitahernandezorg/actions/secrets/{secret_name}"
+
+get_key_id_url = f"https://api.github.com/orgs/gitahernandezorg/actions/secrets/public-key"
+
+# Configura los encabezados de la solicitud
+headers = {
+    "Authorization": f"token {token}",
+    "Accept": "application/vnd.github.v3+json"
+}
+
+response = requests.get(get_key_id_url, headers=headers)
+
+key_id_data = response.json()
+
+# Codifica el valor del secreto en base64
+secret_value_base64 = base64.b64encode(secret_value.encode()).decode()
+
+# Configura el cuerpo de la solicitud
+data = {
+    "encrypted_value": secret_value_base64,
+    "visibility":"selected",
+    "key_id": key_id_data['key_id']
+}
+
+# Realiza la solicitud POST para crear el secreto
+response = requests.put(url, json=data, headers=headers)
+
+# Verifica la respuesta de la solicitud
+if response.status_code == 201:
+    print(f"Secrete was created: {secret_name}")
+else:
+    print(f"No se pudo crear el secreto. Código de estado: {response.status_code}")
+    print(f"Respuesta: {response}")
