74da89549f70ec8f6cd92fbf344fdb5b6e055453
Felipe Hernandez <fhernandez@beyondtrust.com>
2023-09-10 17:51:21 -0500

    test

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import uuid
 
 print("Python Execution")
 token = os.getenv('INPUT_GITHUB_TOKEN')
@@ -11,15 +12,23 @@ def set_github_action_output(output_name, output_value):
     f = open(os.path.abspath(os.environ["GITHUB_OUTPUT"]), "a")
     f.write(f'{output_name}={output_value}')
     f.close()
- 
 
+def set_multiline_output(name, value):
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+        delimiter = uuid.uuid1()
+        print(f'{name}<<{delimiter}', file=fh)
+        print(value, file=fh)
+        print(delimiter, file=fh)
+
+"""
 num = 2 * 2
 set_github_action_output('test_variable', num)
+"""
 
 multiline = "geeksy\
 for\
 geeks"
-set_github_action_output('multiline', multiline)
+set_multiline_output('multiline', multiline)
 
 
 
